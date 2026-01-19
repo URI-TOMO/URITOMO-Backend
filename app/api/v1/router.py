9db4a8d492e39932b89a2a020ee1cab237dcd304
Ohjackson <wogus3673@naver.com>
2026-01-20 02:21:11 +0900

    feat: add example token authentication endpoint for user information retrieval #21

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -5,13 +5,16 @@ API v1 Router
 from fastapi import APIRouter
 
 from app.example.router import router as example_router
+from app.example.token.router import router as example_token_router
 from app.api.v1.user.main import router as main_router
 from app.api.v1.user.setup_mock import router as setup_mock_router
 
 api_router = APIRouter()
 
-# Include example CRUD router
+# Include example routers
 api_router.include_router(example_router)
+api_router.include_router(example_token_router)
+
 
 # Include user routers
 api_router.include_router(main_router)
